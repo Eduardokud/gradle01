8078aeda7fc2b8eb526f497b40b28f5869b5be04
Eduard Kudlanov <eduardokud@mail.ru>
2022-10-11 11:58:41 +0300

    reformat

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -9,7 +9,6 @@ import static org.junit.Assert.assertEquals;
 public class CashbackHackServiceTest {
 
 
-
     @Test
     public void underLimit() {
         int expected = 100;
@@ -18,17 +17,19 @@ public class CashbackHackServiceTest {
         assertEquals(expected, actual);
 
     }
+
     @Test
     public void moreLimit() {
-        int expected =800;
+        int expected = 800;
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1200);
         assertEquals(expected, actual);
 
     }
+
     @Test
     public void equalLimit() {
-        int expected =0;
+        int expected = 0;
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1000);
         assertEquals(expected, actual);
