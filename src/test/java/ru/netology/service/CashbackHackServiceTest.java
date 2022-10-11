57aa2092b1658f9178d855a4eb50ecea8f0629f8
Eduard Kudlanov <eduardokud@mail.ru>
2022-10-11 11:46:13 +0300

    testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,29 @@
+package ru.netology.service;
+
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class CashbackHackServiceTest {
+
+
+
+    @Test
+    public void underLimit() {
+        int expected = 100;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(900);
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    public void moreLimit() {
+        int expected =800;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1200);
+        assertEquals(expected, actual);
+
+    }
+}
+
